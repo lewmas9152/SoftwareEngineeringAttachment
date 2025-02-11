19aea9d2c2589c26f3e9bf675f6326e26baed4e5
lewmas9152 <smnjuguna22@gmail.com>
2025-02-11 07:50:52 +0300

    completed assignment2

--- JS/4.logicalOperators/assignment2.js
+++ JS/4.logicalOperators/assignment2.js
@@ -1,75 +1,77 @@
-// // 1. Check if a string is a palindrome
+// 1. Check if a string is a palindrome
 
-// function isPalindrome(str) {
-//   let string = str.split(" ").join("").replaceAll(/[, ? !]/g, "").toLowerCase();
-//   let result = string.split("").reverse().join("");
-//   return result === string;
-// }
-// console.log(isPalindrome("A man, a plan, a canal, Panama"));
-// console.log(isPalindrome("Was it a car or a cat I saw ?"));
-// console.log(isPalindrome("Hello, World!"))
+function isPalindrome(str) {
+  let string = str
+    .split(" ")
+    .join("")
+    .replaceAll(/[, ? !]/g, "")
+    .toLowerCase();
+  let result = string.split("").reverse().join("");
+  return result === string;
+}
+console.log(isPalindrome("A man, a plan, a canal, Panama"));
+console.log(isPalindrome("Was it a car or a cat I saw ?"));
+console.log(isPalindrome("Hello, World!"));
 
-// // Reverse a string
-// const reverser = (str) => {
-//     let result = str.split("").reverse().join("")
-//     console.log(str)
-//     return result
-// }
+//2 Reverse a string
+const reverser = (str) => {
+  let result = str.split("").reverse().join("");
+  console.log(str);
+  return result;
+};
 
-// console.log(reverser("Kelvin"))
+console.log(reverser("Kelvin"));
 
-// // 3 find the longest palindromic substring
-// function longestPalindromicSubstring(s){
-//     let results = '';
-//     let palindromes = [];
-//     let string = s.split("")
-//     for(let i = 0; i < string.length; i++){
-//         for(let j = 0; j < string.length; j++){
-//             let temp = string.slice(i, j+1).join("");
-//             if(temp.length >= results.length  && isPalindrome(temp)){
-//                 results = temp;
-//                 palindromes.push(temp);
-//             }
+// 3 find the longest palindromic substring
+function longestPalindromicSubstring(s) {
+  let results = "";
+  let palindromes = [];
+  let string = s.split("");
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < string.length; j++) {
+      let temp = string.slice(i, j + 1).join("");
+      if (temp.length >= results.length && isPalindrome(temp)) {
+        results = temp;
+        palindromes.push(temp);
+      }
+    }
+  }
+  return results;
+}
+console.log(longestPalindromicSubstring("babad"));
+console.log(longestPalindromicSubstring("cbbd"));
 
-//         }
-//     }
-//     return results;
+// 4 Check if Strings are Anagrams
+function anagrams(str1, str2) {
+  if (str1.length !== str2.length) return false;
 
-// }
-// console.log( longestPalindromicSubstring('babad') );
-// console.log( longestPalindromicSubstring('cbbd') );
+  let charCount = {};
 
-// // 4 Check if Strings are Anagrams
-// function anagrams(str1, str2) {
-//   if (str1.length !== str2.length) return false;
+  for (let char of str1) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
 
-//   let charCount = {};
+  for (let char of str2) {
+    if (!charCount[char]) return false;
+    charCount[char]--;
+  }
 
-//   for (let char of str1) {
-//     charCount[char] = (charCount[char] || 0) + 1;
-//   }
+  return true;
+}
 
-//   for (let char of str2) {
-//     if (!charCount[char]) return false;
-//     charCount[char]--;
-//   }
-
-//   return true;
-// }
-
-// console.log(anagrams("listen", "silent")); // Output: true
-// console.log(anagrams("hello", "world")); // Output: false
+console.log(anagrams("listen", "silent")); // Output: true
+console.log(anagrams("hello", "world")); // Output: false
 
 //5 Remove duplicates from string
 function removeDuplicates(str) {
   let result = "";
   let string = str.toLowerCase().split("");
   for (let i = 0; i < string.length; i++) {
-    if(!(result.includes(string[i]))){
-       result += string[i]
+    if (!result.includes(string[i])) {
+      result += string[i];
     }
   }
-  return result
+  return result;
 }
 
 console.log(removeDuplicates("programming"));
@@ -78,27 +80,65 @@ console.log(removeDuplicates("aaaa"));
 console.log(removeDuplicates("abcd"));
 console.log(removeDuplicates("aabbcc"));
 
-
 // 6 Count  Palindromes in a string
+function countPalindromes(s) {
+  let palindromes = new Set();
 
-
-//7 longest common prefix
-function longestCommonPrefix(arr){
-    for(let i=0; i< arr.length; i++){
-        for(let j=0; j< arr.length; j++){
-            
-        }
+  function expandAroundCenter(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      palindromes.add(s.substring(left, right + 1));
+      left--;
+      right++;
     }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandAroundCenter(i, i);
+    expandAroundCenter(i, i + 1);
+  }
+
+  return palindromes.size;
 }
 
-let array = ["flower", "flow" , "flight"]
+console.log(countPalindromes("ababa"));
+console.log(countPalindromes("racecar"));
+console.log(countPalindromes("aabb"));
+console.log(countPalindromes("a"));
+console.log(countPalindromes("abc"));
 
-console.log(longestCommonPrefix(['flower','flow','flight']))
-console.log(longestCommonPrefix(['dog','racecar','car']))
-console.log(longestCommonPrefix(['interspecies','interstellar','interstate']))
-console.log(longestCommonPrefix(['prefix','prefixes','preform']))
-console.log(longestCommonPrefix(['apple','banana','cherry']))
+//7 longest common prefix
+function longestCommonPrefix(arr) {
+  if (!arr.length) return "";
 
+  let prefix = arr[0];
 
+  for (let i = 1; i < arr.length; i++) {
+    while (arr[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1);
+      if (!prefix) return "";
+    }
+  }
 
+  return prefix;
+}
 
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+console.log(
+  longestCommonPrefix(["interspecies", "interstellar", "interstate"])
+);
+console.log(longestCommonPrefix(["prefix", "prefixes", "preform"]));
+console.log(longestCommonPrefix(["apple", "banana", "cherry"]));
+
+//8 case Insensitive Palindrome
+function isCaseInsensitivePalindrome(str) {
+  str = str.toLowerCase();
+  let result = str.split("").reverse().join("");
+  return str == result;
+}
+
+console.log(isCaseInsensitivePalindrome("Aba"));
+console.log(isCaseInsensitivePalindrome("Racecar"));
+console.log(isCaseInsensitivePalindrome("Palindrome"));
+console.log(isCaseInsensitivePalindrome("Madam"));
+console.log(isCaseInsensitivePalindrome("Hello"));
